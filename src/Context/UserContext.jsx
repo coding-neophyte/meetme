0bcf8131c37c4ef0bfbc3db84b5e4d5058289892
Julius Franklin <jjfranklin88@outlook.com>
2021-12-13 19:10:05 -0500

    fixing provider

--- src/Context/UserContext.jsx
+++ src/Context/UserContext.jsx
@@ -3,7 +3,7 @@ import { useContext, useState, createContext } from "react";
 const UserContext = createContext()
 
 const UserProvider = ({ children }) => {
-    const [user, setUser] = useState('')
+    const [user, setUser] = useState({})
 
     return <UserContext.Provider value={{ user, setUser }}>
         {children}
